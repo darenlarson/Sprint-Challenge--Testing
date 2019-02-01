146ecbb654e7014c50809284839b42d4408e99ba
darenlarson <larson.daren@gmail.com>
2019-02-01 10:23:26 -0600

    Tests checking GET to /games responding with an array. Tests checking POST to /games responding with 422 when request lacks all required fields, responding with 201 when successfulling adding game, and responding with correct message in response body.

--- api/server.spec.js
+++ api/server.spec.js
@@ -9,5 +9,50 @@ describe('server.js', () => {
 
             expect(response.status).toBe(200);
         });
+
+        it('should return an array, even if there are no games to return', async () => {
+            // const expected = typeof Array; // I don't think this is right. Need to come back to it.
+
+            let response = await request(server).get('/games');
+
+            expect(response.body).toBe(typeof Array);
+
+        });
+    });
+
+    describe('POST /games endpoint', () => {
+        it('should respond with code 422 when required fields are not included in body', async () => {
+            // The body is missing the genre key, which is required.
+            const body = { title: 'Pacman' }
+
+            let response = await request(server)
+                .post('/games')
+                .send(body);
+
+            expect(response.status).toBe(422);
+        });
+
+        it('should respond with code 201', async () => {
+            // The body includes all of the required fields.
+            const body = { title: 'Pacman', genre: 'Arcade', releaseYear: 1980 };
+
+            let response = await request(server)
+                .post('/games')
+                .send(body);
+
+            expect(response.status).toBe(201);
+        });
+
+        it('should return response { message: "Game successfully added }', async () => {
+            // The body includes all of the required fields.
+            const body = { title: 'Pacman', genre: 'Arcade', releaseYear: 1980 };
+            const expected = { message: 'Game successfully added' };
+
+            let response = await request(server)
+                .post('/games')
+                .send(body);
+
+            expect(response.body).toEqual(expected);
+        });
     });
 });
